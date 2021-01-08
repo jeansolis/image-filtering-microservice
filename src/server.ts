7104803599704eac551e94d04cc74bfead9c3926
jeansolis <jeansolisa@gmail.com>
2021-01-08 18:01:41 +0100

    Clean up TODO and instructions

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
 import bodyParser from 'body-parser';
 import express from 'express';
-import http from 'http'
-import url from 'url'
 import {deleteLocalFiles, filterImageFromURL, isValidImageUrl} from './util/util';
 
 (async () => {
@@ -15,21 +13,12 @@ import {deleteLocalFiles, filterImageFromURL, isValidImageUrl} from './util/util
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-  
+  //    the filtered image file
   app.get(`/filteredimage`, async (req, res) => {
     const { image_url } = req.query
     
@@ -64,7 +53,6 @@ import {deleteLocalFiles, filterImageFromURL, isValidImageUrl} from './util/util
     })
     return res.sendFile(filteredImage)
   })
-  //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
